2d6a6d01b8d000330978ec6fb33f5fbec4d8752f
PBAniket <aniketpusarla@gmail.com>
2025-07-15 09:53:24 +0530

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -5,7 +5,7 @@ import os
 import google.generativeai as genai
 
 
-GOOGLE_API_KEY = "AIzaSyAjCd35y22JJRu7C3wA0j0r6j7FU2GUEEs"
+GOOGLE_API_KEY = "your api key"
 GOOGLE_MODEL_NAME = "gemini-1.5-flash-latest"
 
 gemini_model = None
